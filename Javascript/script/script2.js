0784aa1c5937a4dcf762b4e76c476093479851d8
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-10-04 22:09:23 +0530

    ,

--- Javascript/script/script2.js
+++
@@ -1,5 +0,0 @@
-function sortString() {
-    const inputString = document.getElementById('inputString').value;
-    const sortedString = inputString.split('').sort().join('');
-    document.getElementById('result').innerText = `Sorted String: ${sortedString}`;
-}
